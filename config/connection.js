97dd8eed1b5c7d772b8b2cd147997e4006e38da7
Max <mhandler1991@gmail.com>
2019-09-24 17:55:38 -0700

    +SQL connection

--- config/connection.js
+++ config/connection.js
@@ -1,2 +1,22 @@
+// Setup MySQL Connection
+var sql = require('mysql');
+
+// IMPORTANT: PASSWORDS
+// **********************************
+// **********************************
+// Passwords File
+var keys = require('./password.js');
+// My SQL Password
+var password = keys.SQLPassword;
+// **********************************
+// **********************************
+
+var connection = mysql.createconnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password
+})
+
 // Export connection for our ORM to use.
 module.exports = connection;
