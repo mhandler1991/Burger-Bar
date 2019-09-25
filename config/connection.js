af5d8ec0f10c39d1b89bb5148a00ad69288b12d4
Max <mhandler1991@gmail.com>
2019-09-24 17:59:26 -0700

    sql sever update

--- config/connection.js
+++ config/connection.js
@@ -16,7 +16,15 @@ var connection = mysql.createconnection({
     port: 3306,
     user: 'root',
     password
-})
+});
+
+connection.connect(function(err) {
+    if (err) {
+        console.error('error connecting: ', err.stack);
+        return;
+    }
+    console.log('connected as id: ', connection.threadId)
+});
 
 // Export connection for our ORM to use.
 module.exports = connection;
