52b25f8ad79b9cee78e9d5022b81c1c00c466cc2
Max <mhandler1991@gmail.com>
2019-09-28 15:08:47 -0700

    updated seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,5 @@
+USE burgers_db
+
 INSERT INTO burgers (burger_name, devoured) VALUES ('Standard', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Blue Cheese', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Upside Down', false);
